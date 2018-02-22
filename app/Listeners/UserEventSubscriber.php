c8bde2bd44c52fee8218a29afd824dd7463be89e
Carlos Ferreira <carlos@especializati.com.br>
2018-02-22 10:52:17 -0300

    Registra token_access login e cria sessão

--- app/Listeners/UserEventSubscriber.php
+++ app/Listeners/UserEventSubscriber.php
@@ -4,5 +4,37 @@ namespace App\Listeners;
 
 class UserEventSubscriber
 {
-    
+    /**
+     * Handle user login events.
+     */
+    public function onUserLogin($event)
+    {
+    	$tokenAccess = bcrypt(date('YmdHms'));
+
+        $user = auth()->user();
+        $user->token_access = $tokenAccess;
+        $user->save();
+
+        session()->put('access_token', $tokenAccess);
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param  Illuminate\Events\Dispatcher  $events
+     */
+    public function subscribe($events)
+    {
+        $events->listen(
+            'Illuminate\Auth\Events\Login',
+            'App\Listeners\UserEventSubscriber@onUserLogin'
+        );
+ 
+        /*
+        $events->listen(
+            'Illuminate\Auth\Events\Logout',
+            'App\Listeners\UserEventSubscriber@onUserLogout'
+        );
+        */
+    }
 }
